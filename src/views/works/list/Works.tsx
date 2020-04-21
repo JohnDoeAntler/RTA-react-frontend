69865dde5374cb3b368ee37a4c26b97b70abb168
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-04-22 06:10:23 +0800

    adjust list work page.

--- src/views/works/list/Works.tsx
+++ src/views/works/list/Works.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-apollo";
 import { GetWorks, GetWorksVariables } from "../../../types/api";
 import { GET_WORKS } from "../../../graphql/works";
@@ -10,13 +10,14 @@ import "./Works.css";
 import { WorkItem } from "../../../components/WorkItem/WorkItem";
 import { CircleButton } from "../../../components/CircleButton/CircleButton";
 import { Add, ArrowForwardIos, ArrowBackIos, ArrowLeft, ArrowRight } from "@material-ui/icons";
+import { WorkList } from "../../../components/WorkList/WorkList";
 
 interface IWorksProps {}
 
 export const Works: React.FC<IWorksProps> = (props) => {
+
 	const [state, setState] = useState({
 		filter: "",
-		index: 0,
 	});
 
 	const { data, loading } = useQuery<GetWorks, GetWorksVariables>(GET_WORKS, {
@@ -86,52 +87,7 @@ export const Works: React.FC<IWorksProps> = (props) => {
 				>
 					<Grid container direction="column" spacing={2}>
 						<Grid item>
-							{ !data && loading && <div>loading</div>}
-							{
-								data && (
-									<div>
-										<WorkItem
-											key={data.works[state.index % data.works.length].id}
-											{...data.works[state.index % data.works.length]}
-											to={`/works/${data.works[state.index % data.works.length].id}`}
-										/>
-
-										<Grid spacing={1} container alignItems="center">
-											<Grid item>
-												<CircleButton type="button" backgroundColor="black" onClick={() => {
-													setState({
-														...state,
-														index: state.index + 1,
-													});
-												}}>
-													<IconButton>
-														<ArrowLeft/>
-													</IconButton>
-												</CircleButton>
-											</Grid>
-											<Grid item>
-												{
-													(state.index % data.works.length) + 1
-												}/{
-													data.works.length
-												}
-											</Grid>
-											<Grid item>
-												<CircleButton type="button" backgroundColor="black" onClick={() => {
-													setState({
-														...state,
-														index: state.index + 1,
-													})
-												}}>
-													<IconButton>
-														<ArrowRight/>
-													</IconButton>
-												</CircleButton>
-											</Grid>
-										</Grid>
-									</div>
-								)
-							}
+							<WorkList data={data} loading={loading}/>
 						</Grid>
 					</Grid>
 				</Grid>

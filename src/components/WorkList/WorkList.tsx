4afddda5461ca0f5336464744fc3f50e11eeccc7
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-22 06:10:23 +0800

    adjust list work page.

---
+++ src/components/WorkList/WorkList.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { GetWorks } from '../../types/api';
+import { WorkItem } from '../WorkItem/WorkItem';
+import { Grid } from '@material-ui/core';
+import gsap, { Power4 } from 'gsap';
+
+interface IWorkListProps {
+	data?: GetWorks;
+
+	loading: boolean;
+}
+
+export const WorkList: React.FC<IWorkListProps> = ({data, loading}) => {
+
+	const [index, setIndex] = useState(0);
+
+	const wrapper = useRef<HTMLDivElement>(null);
+
+	return (
+		<div
+			ref={wrapper}
+			onWheel={(e) => {
+				if (e.deltaY > 0 && index + 3 < (data?.works.length || 0)) {
+					setIndex(index + 1);
+					gsap.fromTo(
+						wrapper.current,
+						0.5,
+						{
+							y: 50,
+							ease: Power4.easeOut
+						},
+						{
+							y: 0,
+							ease: Power4.easeOut
+						}
+					);
+				} else if (e.deltaY < 0 && index > 0) {
+					setIndex(index - 1);
+					gsap.fromTo(
+						wrapper.current,
+						0.5,
+						{
+							y: -50,
+							ease: Power4.easeOut
+						},
+						{
+							y: 0,
+							ease: Power4.easeOut
+						}
+					);
+				}
+			}}
+		>
+			{
+				!data && loading && "loading..."
+			}
+			{
+				data && (
+					<Grid
+						container
+						direction="column"
+						spacing={1}
+					>
+						{
+							data && data.works.slice(index, index + 3).map((el) => (
+								<Grid item>
+									<WorkItem
+										key={el.id}
+										{...el}
+										to={`/works/${el.id}`}
+									/>
+								</Grid>
+							))
+						}
+					</Grid>
+				)
+			}
+		</div>
+	)
+}

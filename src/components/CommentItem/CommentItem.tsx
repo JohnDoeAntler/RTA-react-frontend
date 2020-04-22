073a2568ddaf8132c6c7dbf17debbada7714efb2
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-04-23 03:32:08 +0800

    comment list and comment item.

---
+++ src/components/CommentItem/CommentItem.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './CommentItem.css';
+import { GetWork_works_by_pk_comments } from '../../types/api';
+import { Link } from 'react-router-dom';
+import { Avatar, Grid } from '@material-ui/core';
+
+export const CommentItem: React.FC<GetWork_works_by_pk_comments> = (props) => {
+	return (
+		<div className="comment-wrapper">
+			<div className="comment-border">
+				<p>
+					{
+						props.content
+					}
+				</p>
+				
+
+				<hr/>
+
+				<small>
+						<Grid
+							className="comment-user-wrapper"
+							container
+							spacing={1}
+							alignItems="center"
+						>
+							<Grid item>
+								<Link to={`/users/${props.user.id}`}>
+									<Avatar className="comment-user-icon" src={props.user.imageUrl}/>
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link to={`/users/${props.user.id}`}>
+									<span className="comment-user">
+										{
+											props.user.name
+										}
+									</span>
+								</Link>
+								{
+									props.created_at
+								}
+							</Grid>
+						</Grid>
+				</small>
+			</div>
+		</div>
+	)
+}

11d93c8250e703026b4449e76931e917151394c1
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-24 04:44:59 +0800

    implement ui on favourite page.

--- src/views/favourites/Favourites.tsx
+++ src/views/favourites/Favourites.tsx
@@ -4,6 +4,10 @@ import { GetFavourites, GetFavouritesVariables } from '../../types/api';
 import { GET_FAVOURITES } from '../../graphql/favourites';
 import { useAuth0 } from '../../utils/react-auth0-spa';
 import { Link } from 'react-router-dom';
+import { WorkList } from '../../components/WorkList/WorkList';
+import { Container, Grid, TextField, IconButton } from '@material-ui/core';
+import { CircleButton } from '../../components/CircleButton/CircleButton';
+import { Add } from '@material-ui/icons';
 
 interface IFavouritesProps {
 };
@@ -16,7 +20,7 @@ export const Favourites: React.FC<IFavouritesProps> = (props) => {
 		filter: '',
 	})
 
-	const { data, loading } = useQuery<GetFavourites, GetFavouritesVariables>(GET_FAVOURITES, {
+	const { data } = useQuery<GetFavourites, GetFavouritesVariables>(GET_FAVOURITES, {
 		variables: {
 			id,
 			filter: `%${state.filter}%`,
@@ -24,30 +28,66 @@ export const Favourites: React.FC<IFavouritesProps> = (props) => {
 		fetchPolicy: 'no-cache',
 	});
 
+	
+
 	return (
-		<div>
-			Favourites works.
+		<Container>
+			<Grid
+				container
+				alignItems="center"
+				spacing={4}
+				style={{
+					height: "100vh",
+				}}>
+				<Grid item xs={12} sm={6}>
+					<Grid container direction="column" spacing={2}>
+						<Grid item>
+							<span className="title-text">Search Favourites</span>
+						</Grid>
 
-			<input type="text" onChange={(e) => {
-				setState({
-					...state,
-					filter: e.currentTarget.value,
-				})
-			}} />
+						<Grid item>
+							<hr />
 
-			{
-				data && data.favourites.map((favourite) => {
-					return (
-						<Link to={`/works/${favourite.work.id}`}>
-							<pre>
-								{
-									JSON.stringify(favourite, null, 4)
-								}
-							</pre>
-						</Link>
-					)
-				})
-			}
-		</div>
+							<span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+						</Grid>
+
+						<Grid item>
+							<TextField
+								fullWidth
+								label="Filter"
+								variant="filled"
+								onChange={(e) => {
+									setState({
+										...state,
+										filter: e.currentTarget.value,
+									});
+								}}
+							/>
+						</Grid>
+
+						<Grid item>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit officiis fugit culpa architecto sapiente, quas maiores dolorem sint,
+							expedita esse, nihil ducimus. Hic obcaecati ex, tempore animi odit quo iusto.
+						</Grid>
+					</Grid>
+				</Grid>
+
+				<Grid
+					item
+					sm={6}
+					xs={12}
+				>
+					<Grid container direction="column" spacing={2}>
+						<Grid item>
+							{
+								data && (
+									<WorkList works={data.favourites.map(x => x.work).map(x => x)}/>
+								)
+							}
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Container>
 	);
 };

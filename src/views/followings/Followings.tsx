c1f782d1731ae42f1418000eda5855f5ea2d0f67
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-24 05:49:54 +0800

    implement ui on followings.

--- src/views/followings/Followings.tsx
+++ src/views/followings/Followings.tsx
@@ -4,6 +4,8 @@ import { GET_FOLLOWINGS } from '../../graphql/followings';
 import { GetFollowings, GetFollowingsVariables } from '../../types/api';
 import { useAuth0 } from '../../utils/react-auth0-spa';
 import { Link } from 'react-router-dom';
+import { Container, Grid, TextField } from '@material-ui/core';
+import { UserList } from '../../components/UserList/UserList';
 
 interface IFollowingsProps {
 };
@@ -25,29 +27,61 @@ export const Followings: React.FC<IFollowingsProps> = (props) => {
 	});
 
 	return (
-		<div>
-			Followings works.
+		<Container>
+			<Grid
+				container
+				alignItems="center"
+				spacing={4}
+				style={{
+					height: "100vh",
+				}}>
+				<Grid item xs={12} sm={6}>
+					<Grid container direction="column" spacing={2}>
+						<Grid item>
+							<span className="title-text">Search Followings</span>
+						</Grid>
 
-			<input type="text" onChange={(e) => {
-				setState({
-					...state,
-					filter: e.currentTarget.value,
-				})
-			}} />
+						<Grid item>
+							<hr />
 
-			{
-				data && data.users.map((user) => {
-					return (
-						<Link to={`/users/${user.id}`}>
-							<pre>
-								{
-									JSON.stringify(user, null, 4)
-								}
-							</pre>
-						</Link>
-					)
-				})
-			}
-		</div>
+							<span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+						</Grid>
+
+						<Grid item>
+							<TextField
+								fullWidth
+								label="Filter"
+								variant="filled"
+								onChange={(e) => {
+									setState({
+										...state,
+										filter: e.currentTarget.value,
+									});
+								}}
+							/>
+						</Grid>
+
+						<Grid item>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit officiis fugit culpa architecto sapiente, quas maiores dolorem sint,
+							expedita esse, nihil ducimus. Hic obcaecati ex, tempore animi odit quo iusto.
+						</Grid>
+					</Grid>
+				</Grid>
+
+				<Grid
+					item
+					sm={6}
+					xs={12}
+				>
+					<Grid container direction="column" spacing={2}>
+						<Grid item>
+							{
+								data && <UserList users={data.users}/>
+							}
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Container>
 	);
 };

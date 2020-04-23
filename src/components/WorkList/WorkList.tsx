6c8aebf416c697e1b9f7def74304532abd4942c0
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-04-23 21:59:16 +0800

    beautify work list component

--- src/components/WorkList/WorkList.tsx
+++ src/components/WorkList/WorkList.tsx
@@ -1,14 +1,17 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { GetWorks, GetUserDetail_users_by_pk, GetFavourites } from '../../types/api';
+import React, { useState, useRef } from 'react';
+import { GetWorks } from '../../types/api';
 import { WorkItem } from '../WorkItem/WorkItem';
 import { Grid } from '@material-ui/core';
 import gsap, { Power4 } from 'gsap';
+import { ScrollSpy } from '../ScrollSpy/ScrollSpy';
+import './WorkList.css';
 
 interface IWorkListProps {
 
 	data?: GetWorks;
 
 	loading: boolean;
+
 }
 
 export const WorkList: React.FC<IWorkListProps> = ({data, loading}) => {
@@ -18,66 +21,83 @@ export const WorkList: React.FC<IWorkListProps> = ({data, loading}) => {
 	const wrapper = useRef<HTMLDivElement>(null);
 
 	return (
-		<div
-			ref={wrapper}
-			onWheel={(e) => {
-				e.preventDefault();
+		<div className="work-list-wrapper">
 
-				if (e.deltaY > 0 && index + 3 < (data?.works.length || 0)) {
-					setIndex(index + 1);
-					gsap.fromTo(
-						wrapper.current,
-						0.5,
+			<div className="upper-triangle"></div>
+
+			<div className="lower-triangle"></div>
+
+			<div className="list-scroll-spy-wrapper">
+				<ScrollSpy offset={(data?.works.length || 0) > 3 ? index / ((data?.works.length || 3) - 3) : 1} afterColor="black">
+					<div className="scroll-spy-text">
 						{
-							y: 50,
-							ease: Power4.easeOut
-						},
-						{
-							y: 0,
-							ease: Power4.easeOut
-						}
-					);
-				} else if (e.deltaY < 0 && index > 0) {
-					setIndex(index - 1);
-					gsap.fromTo(
-						wrapper.current,
-						0.5,
-						{
-							y: -50,
-							ease: Power4.easeOut
-						},
-						{
-							y: 0,
-							ease: Power4.easeOut
-						}
-					);
+							((data?.works.length || 0) > 3 ? index / ((data?.works.length || 3) - 3) : 1) * 100
+						}%
+					</div>
+				</ScrollSpy>
+			</div>
+
+			<div
+				ref={wrapper}
+				onWheel={(e) => {
+					e.preventDefault();
+	
+					if (e.deltaY > 0 && index + 3 < (data?.works.length || 0)) {
+						setIndex(index + 1);
+						gsap.fromTo(
+							wrapper.current,
+							0.5,
+							{
+								y: 50,
+								ease: Power4.easeOut
+							},
+							{
+								y: 0,
+								ease: Power4.easeOut
+							}
+						);
+					} else if (e.deltaY < 0 && index > 0) {
+						setIndex(index - 1);
+						gsap.fromTo(
+							wrapper.current,
+							0.5,
+							{
+								y: -50,
+								ease: Power4.easeOut
+							},
+							{
+								y: 0,
+								ease: Power4.easeOut
+							}
+						);
+					}
+				}}
+			>
+				{
+					!data && loading && "loading..."
 				}
-			}}
-		>
-			{
-				!data && loading && "loading..."
-			}
-			{
-				data && (
-					<Grid
-						container
-						direction="column"
-						spacing={1}
-					>
-						{
-							data && data.works.slice(index, index + 3).map((el) => (
-								<Grid item>
-									<WorkItem
-										key={el.id}
-										{...el}
-										to={`/works/${el.id}`}
-									/>
-								</Grid>
-							))
-						}
-					</Grid>
-				)
-			}
+				{
+					data && (
+						<Grid
+							container
+							direction="column"
+							spacing={1}
+						>
+							{
+								data && data.works.slice(index, index + 3).map((el) => (
+									<Grid item>
+										<WorkItem
+											key={el.id}
+											{...el}
+											to={`/works/${el.id}`}
+										/>
+									</Grid>
+								))
+							}
+						</Grid>
+					)
+				}
+			</div>
 		</div>
 	)
 }

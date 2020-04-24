f235f0a5791305c7253c1f41d8e82c63d2c24979
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-04-24 19:15:24 +0800

    add effects on all pages.

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,16 +4,20 @@ import React, { createContext, useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./Routes";
 import { Navbar } from "../components/Navbar/Navbar";
+import { BackgroundGrid } from "../components/BackgroundGrid/BackgroundGrid";
+import { GradientPass } from "../components/GradientPass/GradientPass";
 
 interface IRoutesProps {}
 
 export const Router: React.FC<IRoutesProps> = (props) => {
 	return (
-		<>
-			<BrowserRouter>
-				<Navbar />
-				<Routes />
-			</BrowserRouter>
-		</>
+		<BrowserRouter>
+			<GradientPass />
+
+			<Navbar />
+			<Routes />
+
+			<BackgroundGrid />
+		</BrowserRouter>
 	);
 };

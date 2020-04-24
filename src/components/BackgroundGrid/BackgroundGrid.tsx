0d5584a306f670cee9fdceda629a0b8e74461294
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-24 19:15:24 +0800

    add effects on all pages.

---
+++ src/components/BackgroundGrid/BackgroundGrid.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './BackgroundGrid.css';
+
+interface IBackgroundGridProps {
+}
+
+export const BackgroundGrid: React.FC<IBackgroundGridProps> = (props) => {
+
+	return (
+		<div className="background-grid"></div>
+	)
+}
---
+++ src/components/GradientPass/GradientPass.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './GradientPass.css';
+
+interface IGradientProps {
+}
+
+export const GradientPass: React.FC<IGradientProps> = (props) => {
+	return (
+		<div className="gradient-pass"></div>
+	)
+}

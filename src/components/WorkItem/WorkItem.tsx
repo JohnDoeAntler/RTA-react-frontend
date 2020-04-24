d9cd1ab939fea6f0ba3329c081d55240e4e66967
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-25 05:30:22 +0800

    remove link from workitem to worklist.

--- src/components/WorkItem/WorkItem.tsx
+++ src/components/WorkItem/WorkItem.tsx
@@ -3,44 +3,43 @@ import './WorkItem.css';
 import { Link } from 'react-router-dom';
 
 interface IWorkItemProps {
-	to: string;
 
 	name: string;
 
 	description: string;
 
 	imageUrl: string;
+
 }
 
 export const WorkItem: React.FC<IWorkItemProps> = (props) => {
 	return (
 		<div className="work-item">
-			<Link to={props.to}>
-				<div className="work-item-img-wrapper">
-					<img
-						className="work-item-img"
-						src={props.imageUrl}
-					/>
-				</div>
-	
-				<div style={{
-					overflow: 'hidden',
-				}}>
-					<div className="work-item-title">
-						{
-							props.name
-						}
-					</div>
+			<div className="work-item-img-wrapper">
+				<img
+					className="work-item-img"
+					src={props.imageUrl}
+					alt=""
+				/>
+			</div>
 
-					<hr/>
-
-					<div className="work-item-desc">
-						{
-							props.description
-						}
-					</div>
+			<div style={{
+				overflow: 'hidden',
+			}}>
+				<div className="work-item-title">
+					{
+						props.name
+					}
 				</div>
-			</Link>
+
+				<hr/>
+
+				<div className="work-item-desc">
+					{
+						props.description
+					}
+				</div>
+			</div>
 		</div>
 	)
 }

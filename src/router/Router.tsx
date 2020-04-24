518311748343b2679a742ac2647f9f0808747cfe
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-24 20:00:10 +0800

    add clicking wave effects

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,12 +6,14 @@ import { Routes } from "./Routes";
 import { Navbar } from "../components/Navbar/Navbar";
 import { BackgroundGrid } from "../components/BackgroundGrid/BackgroundGrid";
 import { GradientPass } from "../components/GradientPass/GradientPass";
+import { ClickWave } from "../components/ClickWave/ClickWave";
 
 interface IRoutesProps {}
 
 export const Router: React.FC<IRoutesProps> = (props) => {
 	return (
 		<BrowserRouter>
+			<ClickWave/>
 			<GradientPass />
 
 			<Navbar />

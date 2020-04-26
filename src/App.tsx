8f1325952e56e000793d36618414ea635022ae96
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-04-26 19:44:26 +0800

    loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,56 @@
-import React from 'react';
-import './App.css';
-import { useAuth0 } from './utils/react-auth0-spa';
-import { Guest } from './views/guest/Guest';
-import { Router } from './router/Router';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+/** @format */
+
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { useAuth0 } from "./utils/react-auth0-spa";
+import { Guest } from "./views/guest/Guest";
+import { Router } from "./router/Router";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Cursor } from "./components/Cursor/Cursor";
+import { ClickWave } from "./components/ClickWave/ClickWave";
+import { GradientPass } from "./components/GradientPass/GradientPass";
+import { BackgroundGrid } from "./components/BackgroundGrid/BackgroundGrid";
+import { LoadingAnimation } from "./components/LoadingAnimation/LoadingAnimation";
 
 const App = () => {
-  const {
-    role,
-    isInitializing,
-    isAuthenticated,
-    getIdTokenClaims
-  } = useAuth0();
+	const { isAuthenticated, getIdTokenClaims, isInitializing } = useAuth0();
 
-  // loading page.
-  if (isInitializing) {
-    return (
-      <div>loading...</div>
-    );
-  }
+	return (
+		<>
+			<Cursor />
+			<ClickWave />
+			<GradientPass />
+			<LoadingAnimation isInitializing={isInitializing} />
 
-  // console log jwt tokens
-  if (isAuthenticated) {
+			{!isInitializing && (
+				<>
+					{isAuthenticated ? (
+						<ApolloProvider
+							client={
+								new ApolloClient({
+									uri: "http://3.12.129.211:8080/v1/graphql",
+									request: async (operation) => {
+										const token = await getIdTokenClaims();
+										operation.setContext({
+											headers: {
+												Authorization: `Bearer ${token.__raw}`,
+											},
+										});
+									},
+								})
+							}>
+							<Router />
+						</ApolloProvider>
+					) : (
+						<Guest />
+					)}
+				</>
+			)}
 
-    const client = new ApolloClient({
-      uri: 'http://3.12.129.211:8080/v1/graphql',
-      request: async (operation) => {
-        const token = await getIdTokenClaims();
-
-        // console.log(token.__raw);
-
-        operation.setContext({
-          headers: {
-            Authorization: `Bearer ${token.__raw}`,
-          },
-        });
-      },
-    });
-
-    return (
-      <ApolloProvider client={client}>
-        <Router/>
-      </ApolloProvider>
-    )
-  } else {
-    return <Guest/>;
-  }
-}
+			<BackgroundGrid />
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/LoadingAnimation/LoadingAnimation.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect } from 'react';
+import Lottie from 'lottie-web';
+
+interface ILoadingAnimationProps {
+
+	isInitializing: boolean;
+
+}
+
+let isLoading = true;
+
+export const LoadingAnimation: React.FC<ILoadingAnimationProps> = (props) => {
+
+	const el = useRef<HTMLDivElement>(null);
+	
+	useEffect(() => {
+		
+		const anim = Lottie.loadAnimation({
+			container: el.current as Element,
+			renderer: "svg",
+			loop: false,
+			autoplay: true,
+			path: "/animations/loading.json",
+		});
+
+		anim.addEventListener('complete', () => {
+			if (isLoading) {
+				anim.goToAndPlay(0);
+			}
+		})
+
+		return () => anim.destroy();
+
+	}, []);
+
+	useEffect(() => {
+		if (!props.isInitializing) {
+			isLoading = false;
+		}
+	}, [props.isInitializing]);
+
+	return (
+		<div className="loading-animation" ref={el}></div>
+	)
+}

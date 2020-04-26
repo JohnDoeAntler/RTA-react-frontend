651270935ecbae6c295cc965a91b463544a1b5e3
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-04-26 19:44:26 +0800

    loading animation

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,24 +4,14 @@ import React, { createContext, useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./Routes";
 import { Navbar } from "../components/Navbar/Navbar";
-import { BackgroundGrid } from "../components/BackgroundGrid/BackgroundGrid";
-import { GradientPass } from "../components/GradientPass/GradientPass";
-import { ClickWave } from "../components/ClickWave/ClickWave";
-import { Cursor } from "../components/Cursor/Cursor";
 
 interface IRoutesProps {}
 
 export const Router: React.FC<IRoutesProps> = (props) => {
 	return (
 		<BrowserRouter>
-			<Cursor />
-			<ClickWave />
-			<GradientPass />
-
 			<Navbar />
 			<Routes />
-
-			<BackgroundGrid />
 		</BrowserRouter>
 	);
 };

70b37d89e40ccaa69cfb8ee31786e75c46c58ac0
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-05-09 01:42:29 +0800

    guest page.

--- src/views/guest/Guest.tsx
+++ src/views/guest/Guest.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import { useAuth0 } from '../../utils/react-auth0-spa';
+/** @format */
 
-interface IGuestProps {
-};
+import React from "react";
+import { useAuth0 } from "../../utils/react-auth0-spa";
+import { Grid } from "@material-ui/core";
+import "./Guest.css";
+
+interface IGuestProps {}
 
 export const Guest: React.FC<IGuestProps> = (props) => {
-
-	const {
-		loginWithRedirect,
-	} = useAuth0();
+	const { loginWithRedirect } = useAuth0();
 
 	return (
-		<div>
-			guest works.
-
-			<button onClick={async () => await loginWithRedirect()}>Login</button>
-		</div>
+		<Grid
+			container
+			direction="column"
+			justify="center"
+			alignItems="center"
+			style={{
+				height: "100vh",
+			}}>
+			<Grid item>
+				<div className="guest-button-wrapper">
+					<button className="title-text" onClick={async () => await loginWithRedirect()}>
+						LOGIN
+					</button>
+				</div>
+			</Grid>
+		</Grid>
 	);
 };

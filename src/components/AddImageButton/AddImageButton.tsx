a4286d8008bd99fef2a2d1e10ae59153b32850af
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-05-22 20:25:59 +0800

    add loading animation to the add image button.

--- src/components/AddImageButton/AddImageButton.tsx
+++ src/components/AddImageButton/AddImageButton.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { useAuth0 } from "../../utils/react-auth0-spa";
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, IconButton } from "@material-ui/core";
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, IconButton, LinearProgress, Box } from "@material-ui/core";
 import axios from "axios";
 import { CircleButton } from "../CircleButton/CircleButton";
 import { CallMerge, LibraryAdd } from "@material-ui/icons";
@@ -18,10 +18,12 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 		file: File | null;
 		open: boolean;
 		isError: boolean;
+		isLoading: boolean;
 	}>({
 		file: null,
 		open: false,
 		isError: false,
+		isLoading: false,
 	});
 
 	return (
@@ -35,6 +37,7 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 						...state,
 						open: true,
 						isError: false,
+						isLoading: false,
 					})
 				}>
 				<IconButton>
@@ -53,7 +56,16 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 				aria-labelledby="form-dialog-title">
 				<DialogTitle id="form-dialog-title">Add Image</DialogTitle>
 				<DialogContent>
-					{state.isError ? (
+					{state.isLoading ? (
+						<DialogContentText>
+							<LinearProgress color="secondary" />
+
+							<Box margin="1rem"/>
+
+							Uploading the image training data to the file server, please wait a minute...
+
+						</DialogContentText>
+					) : state.isError ? (
 						<DialogContentText>Please confirm the uploaded file is in png or jpeg format.</DialogContentText>
 					) : (
 						<>
@@ -85,52 +97,64 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 					)}
 				</DialogContent>
 				<DialogActions>
-					<Button
-						color="primary"
-						onClick={() => {
-							setState({
-								...state,
-								open: false,
-							});
-						}}>
-						Cancel
-					</Button>
-					{!state.isError && (
-						<Button
-							color="primary"
-							disabled={!state.file}
-							onClick={() => {
-								if (state.file) {
-									const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/image`;
-									const formData = new FormData();
-									formData.append("id", props.workId || "");
-									formData.append("file", state.file);
-									const config = {
-										headers: {
-											"content-type": "multipart/form-data",
-										},
-									};
-									axios
-										.post(endpoint, formData, config)
-										.then((x) => {
-											props.onAddedImage && props.onAddedImage();
+					{!state.isLoading && (
+						<>
+							<Button
+								color="primary"
+								onClick={() => {
+									setState({
+										...state,
+										open: false,
+									});
+								}}>
+								Cancel
+							</Button>
+							{!state.isError && (
+								<Button
+									color="primary"
+									disabled={!state.file}
+									onClick={() => {
+										if (state.file) {
+											const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/image`;
+											const formData = new FormData();
+											formData.append("id", props.workId || "");
+											formData.append("file", state.file);
+											const config = {
+												headers: {
+													"content-type": "multipart/form-data",
+												},
+											};
+
 											setState({
 												...state,
-												open: false,
-												file: null,
+												isLoading: true,
 											});
-										})
-										.catch(() => {
-											setState({
-												...state,
-												isError: true,
-												file: null,
-											});
-										});
-								}
-							}}>
-							Add Image
-						</Button>
+
+											axios
+												.post(endpoint, formData, config)
+												.then((x) => {
+													props.onAddedImage && props.onAddedImage();
+													setState({
+														...state,
+														open: false,
+														file: null,
+														isLoading: false,
+													});
+												})
+												.catch(() => {
+													setState({
+														...state,
+														isError: true,
+														file: null,
+														isLoading: false,
+													});
+												});
+										}
+									}}>
+									Add Image
+								</Button>
+							)}
+						</>
 					)}
 				</DialogActions>
 			</Dialog>

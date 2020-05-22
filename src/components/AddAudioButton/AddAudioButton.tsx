807cf773ede54229a263bbb3869fa26e94d7b91d
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-05-22 20:12:35 +0800

    apply error message to add image and audio button

--- src/components/AddAudioButton/AddAudioButton.tsx
+++ src/components/AddAudioButton/AddAudioButton.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useState } from "react";
 import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, IconButton } from "@material-ui/core";
 import axios from "axios";
@@ -5,21 +7,20 @@ import { CircleButton } from "../CircleButton/CircleButton";
 import { LibraryAdd } from "@material-ui/icons";
 
 interface IAddAudioButtonProps {
-
 	workId: string;
 
 	onAddedAudio?: () => void;
-
 }
 
 export const AddAudioButton: React.FC<IAddAudioButtonProps> = (props) => {
-
 	const [state, setState] = useState<{
 		file: File | null;
 		open: boolean;
+		isError: boolean;
 	}>({
 		file: null,
 		open: false,
+		isError: false,
 	});
 
 	return (
@@ -32,10 +33,11 @@ export const AddAudioButton: React.FC<IAddAudioButtonProps> = (props) => {
 					setState({
 						...state,
 						open: true,
+						isError: false,
 					})
 				}>
 				<IconButton>
-					<LibraryAdd/>
+					<LibraryAdd />
 				</IconButton>
 			</CircleButton>
 
@@ -50,30 +52,36 @@ export const AddAudioButton: React.FC<IAddAudioButtonProps> = (props) => {
 				aria-labelledby="form-dialog-title">
 				<DialogTitle id="form-dialog-title">Add Audio</DialogTitle>
 				<DialogContent>
-					<DialogContentText>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quisquam dicta repellat tenetur laboriosam sint provident tempore!
-					</DialogContentText>
+					{state.isError ? (
+						<DialogContentText>Please confirm the upload file is in mp3 format.</DialogContentText>
+					) : (
+						<>
+							<DialogContentText>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quisquam dicta repellat tenetur laboriosam sint provident tempore!
+							</DialogContentText>
 
-					<input
-						type="file"
-						id="raised-button-file"
-						accept="audio/*"
-						style={{
-							display: "none",
-						}}
-						onChange={(e) => {
-							setState({
-								...state,
-								file: e.target.files && e.target.files[0],
-							});
-						}}
-					/>
+							<input
+								type="file"
+								id="raised-button-file"
+								accept="audio/*"
+								style={{
+									display: "none",
+								}}
+								onChange={(e) => {
+									setState({
+										...state,
+										file: e.target.files && e.target.files[0],
+									});
+								}}
+							/>
 
-					<label htmlFor="raised-button-file">
-						<Button component="span" fullWidth>
-							{!!state.file ? state.file.name : "Upload"}
-						</Button>
-					</label>
+							<label htmlFor="raised-button-file">
+								<Button component="span" fullWidth>
+									{!!state.file ? state.file.name : "Upload"}
+								</Button>
+							</label>
+						</>
+					)}
 				</DialogContent>
 				<DialogActions>
 					<Button
@@ -86,31 +94,43 @@ export const AddAudioButton: React.FC<IAddAudioButtonProps> = (props) => {
 						}}>
 						Cancel
 					</Button>
-					<Button
-						color="primary"
-						disabled={!state.file}
-						onClick={() => {
-							if (state.file) {
-								const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/audio`;
-								const formData = new FormData();
-								formData.append('id', props.workId || "");
-								formData.append('file', state.file);
-								const config = {
-									headers: {
-										'content-type': 'multipart/form-data',
-									},
-								};
-								axios.post(endpoint, formData, config).then(x => {
-									props.onAddedAudio && props.onAddedAudio();
-									setState({
-										...state,
-										open: false,
-									});
-								});
-							}
-						}}>
-						Add Audio
-					</Button>
+					{!state.isError && (
+						<Button
+							color="primary"
+							disabled={!state.file}
+							onClick={() => {
+								if (state.file) {
+									const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/audio`;
+									const formData = new FormData();
+									formData.append("id", props.workId || "");
+									formData.append("file", state.file);
+									const config = {
+										headers: {
+											"content-type": "multipart/form-data",
+										},
+									};
+									axios
+										.post(endpoint, formData, config)
+										.then((x) => {
+											props.onAddedAudio && props.onAddedAudio();
+											setState({
+												...state,
+												open: false,
+												file: null,
+											});
+										})
+										.catch(() => {
+											setState({
+												...state,
+												isError: true,
+												file: null,
+											});
+										});
+								}
+							}}>
+							Add Audio
+						</Button>
+					)}
 				</DialogActions>
 			</Dialog>
 		</div>
--- src/components/AddImageButton/AddImageButton.tsx
+++ src/components/AddImageButton/AddImageButton.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useState } from "react";
 import { useAuth0 } from "../../utils/react-auth0-spa";
 import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, IconButton } from "@material-ui/core";
@@ -6,21 +8,20 @@ import { CircleButton } from "../CircleButton/CircleButton";
 import { CallMerge, LibraryAdd } from "@material-ui/icons";
 
 interface IAddImageButtonProps {
-
 	workId: string;
 
 	onAddedImage?: () => void;
-
 }
 
 export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
-
 	const [state, setState] = useState<{
 		file: File | null;
 		open: boolean;
+		isError: boolean;
 	}>({
 		file: null,
 		open: false,
+		isError: false,
 	});
 
 	return (
@@ -33,10 +34,11 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 					setState({
 						...state,
 						open: true,
+						isError: false,
 					})
 				}>
 				<IconButton>
-					<LibraryAdd/>
+					<LibraryAdd />
 				</IconButton>
 			</CircleButton>
 
@@ -51,30 +53,36 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 				aria-labelledby="form-dialog-title">
 				<DialogTitle id="form-dialog-title">Add Image</DialogTitle>
 				<DialogContent>
-					<DialogContentText>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quisquam dicta repellat tenetur laboriosam sint provident tempore!
-					</DialogContentText>
+					{state.isError ? (
+						<DialogContentText>Please confirm the uploaded file is in png or jpeg format.</DialogContentText>
+					) : (
+						<>
+							<DialogContentText>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quisquam dicta repellat tenetur laboriosam sint provident tempore!
+							</DialogContentText>
 
-					<input
-						type="file"
-						id="raised-button-file"
-						accept="image/*"
-						style={{
-							display: "none",
-						}}
-						onChange={(e) => {
-							setState({
-								...state,
-								file: e.target.files && e.target.files[0],
-							});
-						}}
-					/>
+							<input
+								type="file"
+								id="raised-button-file"
+								accept="image/*"
+								style={{
+									display: "none",
+								}}
+								onChange={(e) => {
+									setState({
+										...state,
+										file: e.target.files && e.target.files[0],
+									});
+								}}
+							/>
 
-					<label htmlFor="raised-button-file">
-						<Button component="span" fullWidth>
-							{!!state.file ? state.file.name : "Upload"}
-						</Button>
-					</label>
+							<label htmlFor="raised-button-file">
+								<Button component="span" fullWidth>
+									{!!state.file ? state.file.name : "Upload"}
+								</Button>
+							</label>
+						</>
+					)}
 				</DialogContent>
 				<DialogActions>
 					<Button
@@ -87,31 +95,43 @@ export const AddImageButton: React.FC<IAddImageButtonProps> = (props) => {
 						}}>
 						Cancel
 					</Button>
-					<Button
-						color="primary"
-						disabled={!state.file}
-						onClick={() => {
-							if (state.file) {
-								const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/image`;
-								const formData = new FormData();
-								formData.append('id', props.workId || "");
-								formData.append('file', state.file);
-								const config = {
-									headers: {
-										'content-type': 'multipart/form-data',
-									},
-								};
-								axios.post(endpoint, formData, config).then(x => {
-									props.onAddedImage && props.onAddedImage();
-									setState({
-										...state,
-										open: false,
-									});
-								});
-							}
-						}}>
-						Add Image
-					</Button>
+					{!state.isError && (
+						<Button
+							color="primary"
+							disabled={!state.file}
+							onClick={() => {
+								if (state.file) {
+									const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/image`;
+									const formData = new FormData();
+									formData.append("id", props.workId || "");
+									formData.append("file", state.file);
+									const config = {
+										headers: {
+											"content-type": "multipart/form-data",
+										},
+									};
+									axios
+										.post(endpoint, formData, config)
+										.then((x) => {
+											props.onAddedImage && props.onAddedImage();
+											setState({
+												...state,
+												open: false,
+												file: null,
+											});
+										})
+										.catch(() => {
+											setState({
+												...state,
+												isError: true,
+												file: null,
+											});
+										});
+								}
+							}}>
+							Add Image
+						</Button>
+					)}
 				</DialogActions>
 			</Dialog>
 		</div>

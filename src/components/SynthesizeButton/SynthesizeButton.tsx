49673af644f7e5bb1353b4dabfcb6d68257e9100
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-05-22 20:12:08 +0800

    fix font and remove synthesize button if error occurred.

--- src/components/SynthesizeButton/SynthesizeButton.tsx
+++ src/components/SynthesizeButton/SynthesizeButton.tsx
@@ -52,42 +52,40 @@ export const SynthesizeButton: React.FC<ISynthesizeButtonProps> = (props) => {
 				aria-labelledby="form-dialog-title">
 				<DialogTitle id="form-dialog-title">Synthesize</DialogTitle>
 				<DialogContent>
-					{
-						state.isError ? (
-							<p>
-								For work owner: Please confirm the training material on both audio and image is enough to be synthesized.
-								<br/>
-								For user: Please confirm the uploaded video in appropriate format.
-							</p>
-						) : (
-							<>
-								<DialogContentText>
-									Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quisquam dicta repellat tenetur laboriosam sint provident tempore!
-								</DialogContentText>
+					{state.isError ? (
+						<DialogContentText>
+							For work owner: Please confirm the training material on both audio and image is enough to be synthesized.
+							<br />
+							For user: Please confirm the uploaded video in appropriate format.
+						</DialogContentText>
+					) : (
+						<>
+							<DialogContentText>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quisquam dicta repellat tenetur laboriosam sint provident tempore!
+							</DialogContentText>
 
-								<input
-									type="file"
-									id="raised-button-file"
-									accept="video/mp4"
-									style={{
-										display: "none",
-									}}
-									onChange={(e) => {
-										setState({
-											...state,
-											file: e.target.files && e.target.files[0],
-										});
-									}}
-								/>
+							<input
+								type="file"
+								id="raised-button-file"
+								accept="video/mp4"
+								style={{
+									display: "none",
+								}}
+								onChange={(e) => {
+									setState({
+										...state,
+										file: e.target.files && e.target.files[0],
+									});
+								}}
+							/>
 
-								<label htmlFor="raised-button-file">
-									<Button component="span" fullWidth>
-										{!!state.file ? state.file.name : "Upload"}
-									</Button>
-								</label>
-							</>
-						)
-					}
+							<label htmlFor="raised-button-file">
+								<Button component="span" fullWidth>
+									{!!state.file ? state.file.name : "Upload"}
+								</Button>
+							</label>
+						</>
+					)}
 				</DialogContent>
 				<DialogActions>
 					<Button
@@ -100,36 +98,42 @@ export const SynthesizeButton: React.FC<ISynthesizeButtonProps> = (props) => {
 						}}>
 						Cancel
 					</Button>
-					<Button
-						color="primary"
-						disabled={!state.file}
-						onClick={() => {
-							if (state.file) {
-								const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/progress`;
-								const formData = new FormData();
-								formData.append("workId", props.workId || "");
-								formData.append("userId", id);
-								formData.append("file", state.file);
-								const config = {
-									headers: {
-										"content-type": "multipart/form-data",
-									},
-								};
-								axios.post(endpoint, formData, config).then((x) => {
-									setState({
-										...state,
-										open: false,
-									});
-								}).catch(() => {
-									setState({
-										...state,
-										isError: true,
-									})
-								});
-							}
-						}}>
-						Synthesize
-					</Button>
+
+					{!state.isError && (
+						<Button
+							color="primary"
+							disabled={!state.file}
+							onClick={() => {
+								if (state.file) {
+									const endpoint = `${process.env.REACT_APP_FILE_SERVER_ENDPOINT}/progress`;
+									const formData = new FormData();
+									formData.append("workId", props.workId || "");
+									formData.append("userId", id);
+									formData.append("file", state.file);
+									const config = {
+										headers: {
+											"content-type": "multipart/form-data",
+										},
+									};
+									axios
+										.post(endpoint, formData, config)
+										.then((x) => {
+											setState({
+												...state,
+												open: false,
+											});
+										})
+										.catch(() => {
+											setState({
+												...state,
+												isError: true,
+											});
+										});
+								}
+							}}>
+							Synthesize
+						</Button>
+					)}
 				</DialogActions>
 			</Dialog>
 		</div>

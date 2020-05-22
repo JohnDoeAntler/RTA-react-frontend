32d48ab2d06b13ba82c131249232b0a6d4b419b5
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-05-22 10:14:03 +0800

    remove the mouse following effect

--- src/components/BackgroundGrid/BackgroundGrid.tsx
+++ src/components/BackgroundGrid/BackgroundGrid.tsx
@@ -9,24 +9,24 @@ export const BackgroundGrid: React.FC<IBackgroundGridProps> = (props) => {
 
 	const bg = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
+	// useEffect(() => {
 
-		const listener = (e: MouseEvent) => {
-			gsap.to(
-				bg.current,
-				0.5,
-				{
-					backgroundPositionX: e.clientX / 50,
-					backgroundPositionY: e.clientY / 50,
-					ease: Expo.easeOut
-				}
-			)
-		};
+	// 	const listener = (e: MouseEvent) => {
+	// 		gsap.to(
+	// 			bg.current,
+	// 			0.5,
+	// 			{
+	// 				backgroundPositionX: e.clientX / 50,
+	// 				backgroundPositionY: e.clientY / 50,
+	// 				ease: Expo.easeOut
+	// 			}
+	// 		)
+	// 	};
 
-		document.addEventListener('mousemove', listener);
+	// 	document.addEventListener('mousemove', listener);
 
-		return () => document.removeEventListener('mousemove', listener);
-	}, []);
+	// 	return () => document.removeEventListener('mousemove', listener);
+	// }, []);
 
 	return (
 		<div
